4a3f363470fbf6f5b7c8d6b51952cd0e1f01e854
kisei0824 <kisei0824@gmail.com>
2024-09-07 20:07:18 +0900

    コース一覧デザイン修正

--- src/resources/views/courses/index.blade.php
+++ src/resources/views/courses/index.blade.php
@@ -3,9 +3,9 @@
 @section('title', '記事一覧')
 
 @section('content')
-  <div class="container container-custom">
+  <div class="container-custom">
   @foreach($courses as $course)
-    <div class="card mt-3">
+    <div class="card mt-3 mr-3">
       <a href="{{ route('courses.show', ['id'=>$course->id]) }}">
       <div class="card-body d-flex flex-row">
         <i class="fas fa-user-circle fa-3x mr-1"></i>
@@ -30,9 +30,7 @@
 
 <style>
   .container-custom {
-    height: 330px; /* 幅をカスタマイズ */
     display: flex;
-    justify-content: space-around;
     flex-wrap: wrap;
   }
 </style>
