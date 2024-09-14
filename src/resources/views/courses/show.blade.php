d145650d59dbe52b910ea49d2ba09f5d40c36580
kisei0824 <kisei0824@gmail.com>
2024-09-14 21:05:47 +0900

    コースに紐づくコンテンツを表示

--- src/resources/views/courses/show.blade.php
+++ src/resources/views/courses/show.blade.php
@@ -7,11 +7,18 @@
     <div class="card-body pt-0">
         <h3 class="h4 card-title">
             {{ $course->title }}
-        </a>
         </h3>
         <div class="card-text">
         {{ $course->introduction }}
         </div>
+    </div>
+    <div class="card-body pt-0">
+      @foreach ($contents as $content)
+        <h3 class="h4 card-title">
+          <a href="{{ route('contents.index', $course->id) }}">
+          {{ $content->title }}
+        </h3>
+      @endforeach
     </div>
   </div>
 @endsection
