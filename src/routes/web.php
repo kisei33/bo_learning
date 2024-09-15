002f83a9cfdf37bb5bf3feb85c6d91551e39d329
kisei0824 <kisei0824@gmail.com>
2024-09-15 10:46:42 +0900

    ルーティング追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('courses/','App\Http\Controllers\CourseController@index');
 Route::get('courses/show/{id}','App\Http\Controllers\CourseController@show')->name('courses.show');
 
+Route::get('contents/show/{id}','App\Http\Controllers\ContentController@show')->name('contents.show');
+
 Route::get('/admin/courses/create','App\Http\Controllers\CourseController@create')->name('admin.courses.create');
 Route::post('/admin/courses/create','App\Http\Controllers\CourseController@store')->name('courses.store');
